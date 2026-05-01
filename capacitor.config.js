293cd5bdddf9e4f4b707b24b2115b2f92b30f34b
Vahiff <101524724+Vahiff@users.noreply.github.com>
2026-05-01 07:06:10 +0200

    Add Capacitor config

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'com.fleetprestige.app',
+  appName: 'Fleet Prestige',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
